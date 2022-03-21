3338fec8cc457ee79e258a73c8c857f9139dd9ab
Tatiana S <tatyana-selyuk@yandex.com>
2022-03-21 23:40:44 +0200

    Added urls and view-functions

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
-def index():
-    pass
+def index(request):
+    return HttpResponse('Главная страница')
 
-def group_posts():
-    pass
+
+def group_posts(request, slug):
+    return HttpResponse(f'Пост {slug}')
