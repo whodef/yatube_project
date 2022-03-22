0fefddaf95ae633e4d21305c0043247ad6a1cde9
Tatiana S <tatyana-selyuk@yandex.com>
2022-03-22 20:51:54 +0200

    Added index.html template

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -52,10 +52,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube.urls'
 
+# The path to the directory with templates is moved to the variable:
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
