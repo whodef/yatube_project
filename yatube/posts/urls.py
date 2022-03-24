b4d8e47ce76042c1056febbf248c11142fe7bf86
Tatiana S <tatyana-selyuk@yandex.com>
2022-03-24 23:00:40 +0200

    Added more templates

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,11 @@
 # posts/urls.py
 from django.urls import path
-
 from . import views
+
+
+app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group'),
+    path('group/<slug:slug>', views.group_posts, name='group_posts'),
 ]
