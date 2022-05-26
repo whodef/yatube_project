02e04408a702f1d8a062b40b9c8c1859a12fa369
Tatiana S <tatyana-selyuk@yandex.com>
2022-05-26 17:15:04 +0300

    chore: added static and media on prod

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
@@ -37,5 +39,15 @@ if settings.DEBUG:
 
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
+else:
+
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve,
+                kwargs={'document_root': settings.STATIC_ROOT, }),
+        re_path(r'^media/(?P<path>.*)$', serve,
+                kwargs={'document_root': settings.MEDIA_ROOT, }),
+    ]
+
